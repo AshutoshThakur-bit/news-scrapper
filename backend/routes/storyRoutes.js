5e9287ac67fb010f5fe002ca1f1663749c6e3ee0
Your Name <your.email@example.com>
2026-05-07 19:49:36 +0530

    add stories API with bookmark functionality

---
+++ backend/routes/storyRoutes.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const router = express.Router();
+
+const {getAllStories, getStoryById, toggleBookmark,} = require('../controllers/storyController');
+
+const protect = require('../middleware/authMiddleware');
+
+router.get('/', getAllStories);
+router.get('/:id', getStoryById);
+router.get('/:id/bookmark', protect, toggleBookmark);
+
+
+module.exports = router;
